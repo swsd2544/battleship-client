a36a4d35b38c67ebba6791bc21dc80a2f87b125d
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2021-11-12 08:20:22 +0700

    failed badly

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import socketio from "socket.io-client";
 
-const SOCKET_URL = "https://battleship-socket-server.herokuapp.com:4000";
+const SOCKET_URL = "https://battleship-socket-server.herokuapp.com";
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = createContext();
