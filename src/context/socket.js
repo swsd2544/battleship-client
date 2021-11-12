9cdeefd9222e8dde192665890903068ef9f8169d
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2021-11-12 08:29:52 +0700

    Test commit

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
-import socketio from "socket.io-client";
+import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://battleship-socket-server.herokuapp.com";
+const SOCKET_URL = "http://battleship-socket-server.herokuapp.com/";
 
-export const socket = socketio.connect(SOCKET_URL);
+export const socket = io(SOCKET_URL);
 export const SocketContext = createContext();
